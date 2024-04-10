e00c7199f4af3a19a27f27610a990473ea7adce0
Wiencek98 <mrsupplied@gmail.comm>
2024-04-10 14:58:14 -0400

    got a cookie

---
+++ script.js
@@ -0,0 +1,7 @@
+let cookieCount = 0;
+
+document.querySelector('button').addEventListener('click', a => {
+
+    document.querySelector('h2').innerText = 'nice, you got a cookie from the cookie jar'
+
+})
