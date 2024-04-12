b0394a446194fa0a59bed471cda1b967f44d8109
Madison Vizcarra <madisonvizcarra@Madisons-Air.home>
2024-04-12 12:13:14 -0400

    Added some JS and Style

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 let cookieCount = 0;
+let chances = 3;
+let level = 1;
 
-document.querySelector('button').addEventListener('click', a => {
+document.getElementById('buzzer').addEventListener('click', function(){
+    // this reduce chances by 1
+    chances--;
 
-    document.querySelector('h2').innerText = 'nice, you got a cookie from the cookie jar'
+    //no more chances end the game
+    if(changes === 0) {
+        endGame();
+        return;
+    }
+    cookieCount++ // inccreases cookie count by 1 when buzzer is clicked
+    
+    document.getElementById('cookieCount').innerText = cookieCount;
+    if(Math.random() < 0.5) {
+        //increases lvl
+        level++;
 
-})
+        //updated chances
+        chances = 3;
+
+        alert("Congratulations! You hit the buzzer with a cookie and reached level" + level + "!")
+
+        document.getElementById("chances").innerText = chances;
+
+    }
+});
+
+function endGame() {
+    alert("Game over! You've used up all your chances. Your final score is" + cookieCount + "cookies!");
+        cookieCount = 0;
+        chances = 3;
+        level = 1;
+    document.getElementById("cookieCount").innerText - cookieCount;
+    document.getElementById("chances").innerText = changed;
+}
+
+
+endGame()
